c1881fb7622943d925ceeab56b45425d33e9ff69
Raddames Tonui <raddamestonui48@gmail.com>
2025-08-27 13:47:15 +0300

    memory storage

--- session/sessionstorage.js
+++
@@ -1,8 +0,0 @@
-// Session Storage
-function getTickets(){
-  return JSON.parse(sessionStorage.getItem("tickets")) || [];
-}
-
-function saveTickets(tickets) {
-  sessionStorage.setItem("tickets", JSON.stringify(tickets));
-}
